4e14a10b4eea20ee2641684d412fb1affff0c320
Vaibhav Shejol <vaibhav.shejol@bnt-soft.com>
2025-12-01 11:27:44 +0530

    initial package structure

---
+++ HsbcMvpAdminAngularBackend/src/main/java/com/bnt/HsbcMvpAdminAngularBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsbcMvpAdminAngularBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsbcMvpAdminAngularBackendApplication.class, args);
+	}
+
+}
